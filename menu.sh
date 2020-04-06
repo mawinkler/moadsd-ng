414ed800f19afb9caecc0da8118e3e0d1219651c
mawinkler <winkler.info@icloud.com>
2020-04-06 15:39:04 +0200

    now allows to run single deploy_ playbooks

--- menu.sh
+++ menu.sh
@@ -51,13 +51,42 @@ done
 
 pb_options=("site" "deploy" "deploy_endpoints"
             "jenkins_create_credentials" "deploy_gitlab_runners"
-            "pause" "resume" "terminate" "terminate_site")
+            "pause" "resume" "terminate" "terminate_site" "manual")
+
 echo 'Please choose the playbook: '
 select opt in "${pb_options[@]}"
 do
+  # case $opt in
+  #   "manual")
+  #   select manual_pb in "${pb_manual[@]}"
+  #   do
+  #     # case $manual_pb
+  #     opt=$manual_pb
+  #     run_pb=$opt
+  #     break
+  #       # ;;
+  #     # esac
+  #   done
+  #   ;;
+  #   *) run_pb=$opt
+  #   break
+  #   ;;
+  # esac
   run_pb=$opt
   break
 done
+
+if [ $run_pb = "manual" ]
+then
+  pb_manual=( $(find . -maxdepth 1 -name deploy_\*.yml  -exec basename {} .yml \; | sort ) )
+  echo 'Please choose the playbook: '
+  select opt in "${pb_manual[@]}"
+  do
+    # case $manual_pb
+    run_pb=$opt
+    break
+  done
+fi
 
 echo Running ansible-playbook --vault-password-file ../.vault-pass.txt -i ${run_inv} --extra-vars="type=${run_env}" ${run_pb}.yml
 ansible-playbook --vault-password-file ../.vault-pass.txt -i ${run_inv} --extra-vars="type=${run_env}" ${run_pb}.yml
